d5aef155712962512a7554045aed7c4b86cd65f2
codepanther <pezechigozirim@gmail.com>
2021-12-22 04:19:11 +0100

    Add the currency converter calculator functionality

--- src/containers/converter/Converter.js
+++ src/containers/converter/Converter.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { CgArrowRightO } from 'react-icons/cg';
+import './Converter.css';
+import { getConversionRate } from '../../redux/converter/converter';
 
-const Converter = () => (
+const Converter = () => {
+  const [inputValue, setInputValue] = useState(1);
+  const onChangeHandler = (e) => setInputValue(e.target.value);
+  const { from, to } = useParams();
 
-  <div>
-    Converter
-  </div>
+  const { rate } = useSelector((state) => state.ratesReducer);
+  const { baseCurrency } = useSelector((state) => state.currenciesReducer);
 
-);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getConversionRate(from, to));
+  }, []);
+
+  useEffect(() => {
+    dispatch(getConversionRate(baseCurrency, to));
+  }, [dispatch, baseCurrency]);
+
+  return (
+    <div className="convertir__converter">
+      <h2>Use our free converter</h2>
+      <div className="convertir__converter-from flex">
+        <p>{baseCurrency !== from ? baseCurrency : from}</p>
+        <input type="text" value={inputValue} placeholder="1" onChange={onChangeHandler} />
+        <CgArrowRightO />
+      </div>
+      <div className="convertir__converter-to flex">
+        <p>{to}</p>
+        <p>{rate[to] * inputValue}</p>
+        <CgArrowRightO />
+      </div>
+    </div>
+  );
+};
 
 export default Converter;
